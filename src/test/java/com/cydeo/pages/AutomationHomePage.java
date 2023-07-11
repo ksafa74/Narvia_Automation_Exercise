2240077459fc55faa75f478592c761d618014bda
Kasim_Cydeo <safasdet@gmail.com>
2023-07-11 17:20:40 -0400

    Test Case 1 Completed

---
+++ src/test/java/com/cydeo/pages/AutomationHomePage.java
@@ -0,0 +1,24 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AutomationHomePage {
+
+    public AutomationHomePage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[@href=\"/login\"]")
+    public WebElement signupLoginButton;
+
+    @FindBy(xpath = "//a[.=' Delete Account']")
+    public WebElement deleteAccountButton;
+
+    @FindBy(xpath = "//i[@class='fa fa-user']")
+    public WebElement loggedInUser;
+
+}
